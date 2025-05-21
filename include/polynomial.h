c260e58cb82b1ba284c27a13f161eaacfdc38896
ulianamaslova25 <uliana-maslova@mail.ru>
2025-05-21 16:52:13 +0300

    classes

---
+++ include/polynomial.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stdexcept>
+#include <cmath>    
+#include <cctype>   
+#include <sstream>  
+#include <limits>   
+#include <cstdlib>  
+
+const int MIN_DEGREE = 0;
+const int MAX_DEGREE = 9;
+const double EPSILON = 1e-9;
+
+struct Monomial {
+    double coeff;
+    int px, py, pz; 
+
+    
+};
+
+class Polynomial {
+private:
+    std::vector<Monomial> terms; 
+    
+
+
+public:
+    Polynomial() {}
+
+    
+};
+
+
+
+
