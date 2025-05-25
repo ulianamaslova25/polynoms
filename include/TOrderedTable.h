897ccf4d0b265cab9129b04c263eb99eb7f671ff
ulianamaslova25 <uliana-maslova@mail.ru>
2025-05-25 18:38:14 +0300

    added print()

--- include/TOrderedTable.h
+++ include/TOrderedTable.h
@@ -129,6 +129,24 @@ public:
         }
         return it.value();
     }
+    void print() const {
+        std::cout << "--- Упорядоченная таблица (Размер: " << size() << ") ---\n";
+        if (table.empty()) {
+            std::cout << "Table is empty.\n";
+        }
+        else {
+            bool first = true;
+            for (const auto& pair : table) {
+                if (!first) {
+                    std::cout << ", ";
+                }
+                std::cout << "[" << pair.first << ": " << pair.second << "]";
+                first = false;
+            }
+            std::cout << "\n"; 
+        }
+        std::cout << "-------------------------------------------\n";
+    }
 };
 
 
